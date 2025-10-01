0e489f97b23e0acec66aee9871352beefb00c88e
gebysinagaaa________10 <marszllbot21@gmail.com>
2025-10-01 23:15:50 +0700

    Update module.js

--- module.js
+++ module.js
@@ -1,18 +1,27 @@
-module.exports = {
-modul: {
-	axios: require('axios'),
-	boom: require('@hapi/boom'),
-	baileys: require("@whiskeysockets/baileys"),
-	keyeddb: require('@adiwajshing/keyed-db'),
-	os: require('os'),
-	crypto: require('crypto'), 
-	fs: require('fs'), 
-	ffmpeg: require('fluent-ffmpeg'),
-	pino: require('pino'),
-	path: require('path'),
-	url: require('url'),
-	webp: require('node-webpmux'), 
-	PhoneNumber: require('awesome-phonenumber')
+import axios from 'axios'
+import { Boom } from '@hapi/boom'
+import baileys from '@whiskeysockets/baileys'
+import os from 'os'
+import crypto from 'crypto'
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
+import pino from 'pino'
+import path from 'path'
+import url from 'url'
+import webp from 'node-webpmux'
+import PhoneNumber from 'awesome-phonenumber'
 
-}
+export const modul = {
+	axios,
+	boom: Boom,
+	baileys,
+	os,
+	crypto,
+	fs,
+	ffmpeg,
+	pino,
+	path,
+	url,
+	webp,
+	PhoneNumber
 }
