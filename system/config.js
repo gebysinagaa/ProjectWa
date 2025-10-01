bb2ff3a9ff5340239de187a63eaba3b729223d5a
gebysinagaaa________10 <marszllbot21@gmail.com>
2025-10-01 22:47:47 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,27 +1,28 @@
+import fs from "fs"
 
+// ==== INFORMASI BOT ====
 global.info = {
-   ownerName: "𝐈𝐤𝐲𝐲𝐊𝐳𝐲", 
-   ownerNumber: "6281248845231", 
-   botName: "Kazumi Botz", 
+   ownerName: "𝐈𝐤𝐲𝐲𝐊𝐳𝐲",
+   ownerNumber: "6281248845231",
+   botName: "Kazumi Botz",
    botVersion: "1.0.0",
 }
 
 global.mess = {
-   group: "Only Group", 
-   private: "Only Private", 
+   group: "Only Group",
+   private: "Only Private",
    owner: "Only Owner"
 }
 
 global.sticker = {
-   packname: "Sticker Made By", 
+   packname: "Sticker Made By",
    author: "Kazumi"
 }
 
-let fs = require('fs')
-let file = require.resolve(__filename)
+// ==== AUTO RELOAD CONFIG.JS ====
+const file = new URL(import.meta.url).pathname
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(`Update ${__filename}`)
-delete require.cache[file]
-require(file)
+   fs.unwatchFile(file)
+   console.log(`Update ${file}`)
+   import(`${import.meta.url}?update=${Date.now()}`)
 })
